4643ddb435a7a57bfe474d24cd8b1592f28feee9
cyril maitre <cyril.maitre@madebykawet.com>
2013-08-29 17:26:44 +0200

    Les débris sont explusé plus uniformement et plus proprepment

--- src/space_2/WreckMini.cpp
+++ src/space_2/WreckMini.cpp
@@ -14,10 +14,10 @@
 #define SPRITE_COUNT				6
 #define QUICKENING					100
 #define VELOCITY_MIN				250
-#define VELOCITY_MAX				500
+#define VELOCITY_MAX				400
 #define SIZE						32
 #define ROTATION_VELOCITY_MIN		5
-#define ROTATION_VELOCITY_MAX		50
+#define ROTATION_VELOCITY_MAX		10
 
 
 //*************************************************************
@@ -25,10 +25,34 @@
 //*************************************************************
 WreckMini::WreckMini( Wreck* p_parent )
 {
-	this->setRockingActived(false);
 	this->setVelocityMax(Tools::random(VELOCITY_MIN, VELOCITY_MAX));
-	for(int i = 0; i < 8; i++)
-		this->setVelocityAt((MovableCardinality)i, Tools::random(0, this->getVelocityMax()));
+	int vectorDirection = Tools::random(0, 3);
+	if(vectorDirection == 0)	// North-East
+	{
+		this->setVelocityAt(Movable::MovableCardinality::North, Tools::random(0, this->getVelocityMax()));
+		this->setVelocityAt(Movable::MovableCardinality::NorthEast, Tools::random(0, this->getVelocityMax()));
+		this->setVelocityAt(Movable::MovableCardinality::East, Tools::random(0, this->getVelocityMax()));
+	}
+	else if(vectorDirection == 1)	// South-East
+	{
+		this->setVelocityAt(Movable::MovableCardinality::South, Tools::random(0, this->getVelocityMax()));
+		this->setVelocityAt(Movable::MovableCardinality::SouthEast, Tools::random(0, this->getVelocityMax()));
+		this->setVelocityAt(Movable::MovableCardinality::East, Tools::random(0, this->getVelocityMax()));
+	}
+	else if(vectorDirection == 2)	// South-West
+	{
+		this->setVelocityAt(Movable::MovableCardinality::South, Tools::random(0, this->getVelocityMax()));
+		this->setVelocityAt(Movable::MovableCardinality::SouthWest, Tools::random(0, this->getVelocityMax()));
+		this->setVelocityAt(Movable::MovableCardinality::West, Tools::random(0, this->getVelocityMax()));
+	}
+	else	// North-West
+	{
+		this->setVelocityAt(Movable::MovableCardinality::North, Tools::random(0, this->getVelocityMax()));
+		this->setVelocityAt(Movable::MovableCardinality::NorthWest, Tools::random(0, this->getVelocityMax()));
+		this->setVelocityAt(Movable::MovableCardinality::West, Tools::random(0, this->getVelocityMax()));
+	}
+
+	this->setRockingActived(false);
 	this->setQuickening(QUICKENING);
 	this->setSize(SIZE, SIZE);
 	this->setPosition(p_parent->Object::getX(), p_parent->Object::getY());
