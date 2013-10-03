4668b291b0a123ff868083bfe4e995fa513fb804
cyril maitre <cyril.maitre@madebykawet.com>
2013-10-03 13:34:03 +0200

    Refresh new weapon (with good id) after create server side

--- src/space_2/CharacterShipWeaponCreate.cpp
+++ src/space_2/CharacterShipWeaponCreate.cpp
@@ -1,5 +1,12 @@
 #include "CharacterShipWeaponCreate.h"
 #include "NetworkDefine.h"
+#include "Weapon.h"
+
+
+//*************************************************************
+// Define
+//*************************************************************
+#define RESPONSEJSON_CHARACTERSHIP		"charactership"
 
 
 //*************************************************************
@@ -22,9 +29,25 @@ void CharacterShipWeaponCreate::job()
 {
 	if(this->mCharacterShip != NULL)
 	{
+		// Create weapons
 		Json::Value jsonRequest = this->mCharacterShip->saveToJson();
 		Json::StyledWriter writer;
 		sf::Http::Response response = Network::sendJsonRequest(sf::Http::Request::Post, NETWORK_HTTP_URI_CHARACTERSHIP_UPDATE, writer.write(jsonRequest));
+
+		// Update local id
+		Json::Value jsonResponse;   
+		Json::Reader reader;
+		bool parsingSuccessfull = reader.parse(response.getBody(), jsonResponse);
+		if(parsingSuccessfull)
+		{
+			CharacterShip* newShip = new CharacterShip(jsonResponse.get(RESPONSEJSON_CHARACTERSHIP, NULL), NULL);
+			if(newShip->getWeaponsCount() == this->mCharacterShip->getWeaponsCount())
+			{
+				for(int i = 0; i < newShip->getWeaponsCount(); i++)
+					this->mCharacterShip->getWeapon(i)->setIdWeapon(newShip->getWeapon(i)->getIdWeapon());
+			}
+		}
+
 		this->setRunning(false);
 	}
 	else
